49aec334469b61a46d3dbcf0c650097a858fc293
zon <zon.n@outlook.com>
2025-03-21 17:14:20 -0400

    Implémentation de la lecture de l'IMU

--- src/sensor/imu.cpp
+++ src/sensor/imu.cpp
@@ -1,1 +1,50 @@
 #include "imu.h"
+
+void IMU::start()
+{
+    // Initialiser le MPU6050
+    mpu.initialize();
+
+    // Régler le décalage des capteurs
+    mpu.setXAccelOffset(OFFSETS[0]);
+    mpu.setYAccelOffset(OFFSETS[1]);
+    mpu.setZAccelOffset(OFFSETS[2]);
+    mpu.setXGyroOffset(OFFSETS[3]);
+    mpu.setYGyroOffset(OFFSETS[4]);
+    mpu.setZGyroOffset(OFFSETS[5]);
+}
+
+void IMU::read()
+{
+    mpu.getMotion6(&accelX, &accelY, &accelZ, &gyroX, &gyroY, &gyroZ);
+}
+
+int16_t IMU::getAccelX()
+{
+    return accelX;
+}
+
+int16_t IMU::getAccelY()
+{
+    return accelY;
+}
+
+int16_t IMU::getAccelZ()
+{
+    return accelZ;
+}
+
+int16_t IMU::getGyroX()
+{
+    return gyroX;
+}
+
+int16_t IMU::getGyroY()
+{
+    return gyroY;
+}
+
+int16_t IMU::getGyroZ()
+{
+    return gyroZ;
+}
--- src/sensor/imu.h
+++ src/sensor/imu.h
@@ -1,1 +1,29 @@
 #include <Arduino.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+struct IMU
+{
+    static constexpr int16_t OFFSETS[6] = {0, 0, 0, 0, 0, 0};
+    static constexpr int8_t SDA = 9;
+    static constexpr int8_t SCL = 10;
+
+    public:
+        void start();
+        void read();
+        int16_t getAccelX();
+        int16_t getAccelY();
+        int16_t getAccelZ();
+        int16_t getGyroX();
+        int16_t getGyroY();
+        int16_t getGyroZ();
+
+    private:
+        MPU6050 mpu;
+        int16_t accelX;
+        int16_t accelY;
+        int16_t accelZ;
+        int16_t gyroX;
+        int16_t gyroY;
+        int16_t gyroZ;
+};
