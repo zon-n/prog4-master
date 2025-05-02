f3a3afbd03ff915252a5dd377379fdfb7a0b222a
zon <zon.n@outlook.com>
2025-05-02 15:34:53 -0400

    Highchart Implementation

--- data/script.js
+++ data/script.js
@@ -1,25 +1,125 @@
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+window.addEventListener('load', getReadings);
+
+var chartT = new Highcharts.Chart({
+  chart:{
+    renderTo:'chart-telemetry'
+  },
+  series: [
+    {
+      name: 'Throttle % Front Left',
+      type: 'line',
+      color: '#101D42',
+      marker: {
+        symbol: 'circle',
+        radius: 3,
+        fillColor: '#101D42',
+      }
+    },
+    {
+      name: 'Throttle % Front Right',
+      type: 'line',
+      color: '#00A6A6',
+      marker: {
+        symbol: 'square',
+        radius: 3,
+        fillColor: '#00A6A6',
+      }
+    },
+    {
+      name: 'Throttle % Rear Left',
+      type: 'line',
+      color: '#8B2635',
+      marker: {
+        symbol: 'triangle',
+        radius: 3,
+        fillColor: '#8B2635',
+      }
+    },
+    {
+      name: 'Throttle % Rear Right',
+      type: 'line',
+      color: '#71B48D',
+      marker: {
+        symbol: 'triangle-down',
+        radius: 3,
+        fillColor: '#71B48D',
+      }
+    },
+  ],
+  title: {
+    text: undefined
+  },
+  xAxis: {
+    type: 'time',
+    dateTimeLabelFormats: { second: '%S' }
+  },
+  yAxis: {
+    title: {
+      text: 'Throttle Percentage (%)'
+    }
+  },
+  credits: {
+    enabled: false
+  }
+});
+
+
+function plotJSON(jsonValue) {
+
+  var keys = Object.keys(jsonValue);
+  console.log(keys);
+  console.log(keys.length);
+
+  for (var i = 0; i < keys.length; i++){
+    var x = (new Date()).getTime();
+    console.log(x);
+    const key = keys[i];
+    var y = Number(jsonValue[key]);
+    console.log(y);
+
+    if(chartT.series[i].data.length > 40) {
+      chartT.series[i].addPoint([x, y], true, true, true);
+    } else {
+      chartT.series[i].addPoint([x, y], true, false, true);
+    }
+
+  }
 }
 
-setInterval(() => {
-  const now = (new Date()).getTime();
+function getReadings(){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      console.log(myObj);
+      plotJSON(myObj);
+    }
+  };
+  xhr.open("GET", "/readings", true);
+  xhr.send();
+}
 
-  // Les randoms numbers
-  const throttle = getRandom(0, 100);
-  const steering = getRandom(-45, 45); // degrés
-  const wifi = getRandom(-90, -30); // dBm
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
 
-  document.getElementById('throttleDisplay').textContent = `Throttle: ${throttle}`;
-  document.getElementById('steeringDisplay').textContent = `Steering: ${steering}`;
-  document.getElementById('wifiDisplay').textContent = `WiFi: ${wifi} dBm`;
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
 
-  // Les points sur chaque graphique
-  chartT.series[0].addPoint([now, throttle], true, chartT.series[0].data.length > 20);
-  chart2.series[0].addPoint([now, wifi], true, chart2.series[0].data.length > 20);
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
 
-  // Fleche de direction
-  const arrow = document.querySelector('.arrow');
-  arrow.style.transform = `translateX(-50%) rotate(${steering}deg)`;
+  source.addEventListener('message', function(e) {
+    console.log("message", e.data);
+  }, false);
 
-}, 500); // chaque demi-seconde
+  source.addEventListener('new_readings', function(e) {
+    console.log("new_readings", e.data);
+    var myObj = JSON.parse(e.data);
+    console.log(myObj);
+    plotJSON(myObj);
+  }, false);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,109 @@
 #include <Arduino.h>
-#include <web/serveur.h> // Include the Serveur class header    
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
 
-Serveur serveur; // Create an instance of the Serveur class
+// Replace with your network credentials
+const char *ssid = "DESKTOP-MC5O0GI 6923";
+const char *password = "22]j8M81";
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 2000;
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings()
+{
+    readings["sensor1"] = random(0, 10);
+    readings["sensor2"] = random(0, 10);
+    readings["sensor3"] = random(0, 10);
+    readings["sensor4"] = random(0, 10);
+
+    String jsonString = JSON.stringify(readings);
+    return jsonString;
+}
+
+// Initialize LittleFS File System
+void initLittleFS()
+{
+    if (!LittleFS.begin())
+    {
+        Serial.println("An error has occurred while mounting LittleFS");
+    }
+    else
+    {
+        Serial.println("LittleFS mounted successfully");
+    }
+}
+
+// Initialize WiFi
+void initWiFi()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print('.');
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
+}
 
 void setup()
 {
-    Serial.begin(115200); // Initialize serial communication
-    serveur.getIP();       // Get the IP address of the ESP32
-    delay(1000);           // Delay to allow for WiFi connection
+    Serial.begin(115200);
+    serverInit();
+}
+
+void serverInit()
+{
+    initWiFi();
+    initLittleFS();
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/index.html", "text/html"); });
+
+    server.serveStatic("/", LittleFS, "/");
+
+    server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+    String json = getSensorReadings();
+    request->send(200, "application/json", json);
+    json = String(); });
+
+    events.onConnect([](AsyncEventSourceClient *client)
+                     {
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000); });
+    server.addHandler(&events);
+
+    // Start server
+    server.begin();
 }
 
 void loop()
 {
-    serveur.update(); // Update the server
-    delay(100);        // Delay to avoid overwhelming the server
-    // Read the throttle and steering values from the PWM channels
-    uint throttle = random(0, 10); // Simulate throttle input
-    uint steering = random(0, 10); // Simulate steering input
+    serverUpdate();
+}
 
-    serveur.updateTelemetry(throttle, steering); // Update telemetry data
+void serverUpdate()
+{
+    if ((millis() - lastTime) > timerDelay)
+    {
+        // Send Events to the client with the Sensor Readings Every 10 seconds
+        events.send("ping", NULL, millis());
+        events.send(getSensorReadings().c_str(), "new_readings", millis());
+        lastTime = millis();
+    }
 }
