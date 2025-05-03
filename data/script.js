4abc9d082d95589c07736950dbb532452c6300f5
Dan <zon.n@outlook.com>
2025-05-02 23:54:49 -0400

    Conversion de Vecteurs à Mécanums

--- data/script.js
+++ data/script.js
@@ -1,11 +1,12 @@
+// Initialisation de l'affichage
 window.addEventListener("load", getReadings);
 
-var chartT = new Highcharts.Chart({
+// Configuration des diagrammes
+var chart1 = new Highcharts.Chart({
   chart: {
     type: "bar",
     renderTo: "chart-telemetry",
   },
-
   series: [
     {
       name: "Vitesse input",
@@ -34,46 +35,60 @@ var chartT = new Highcharts.Chart({
     text: undefined,
   },
   xAxis: {
-    categories: ["Avant Gauche", "Avant Droit", "Arrière Gauche", "Arrière Droit"],
+    categories: [
+      "Avant Gauche",
+      "Avant Droit",
+      "Arrière Gauche",
+      "Arrière Droit",
+    ],
   },
   yAxis: {
     title: {
       text: "Throttle Percentage (%)",
     },
-    min: 0, // Set the minimum value
-    max: 100, // Set the maximum value
+    min: 0,
+    max: 150,
   },
-
   credits: {
     enabled: false,
   },
 });
 
+// Fontions de logique Javascript
 function plotJSON(jsonValue) {
+  mecanumSpeedInput = vectorToMecanum(
+    Number(jsonValue["input_x"]),
+    Number(jsonValue["input_y"])
+  );
+
+  mecanumSpeedOutput = vectorToMecanum(
+    Number(jsonValue["output_x"]),
+    Number(jsonValue["output_y"])
+  );
+
   var wheelData = {
     input: {
-      front_left: Number(jsonValue["front_left_input"]),
-      front_right: Number(jsonValue["front_right_input"]),
-      rear_left: Number(jsonValue["rear_left_input"]),
-      rear_right: Number(jsonValue["rear_right_input"]),
+      front_left: mecanumSpeedInput[0],
+      front_right: mecanumSpeedInput[1],
+      rear_left: mecanumSpeedInput[2],
+      rear_right: mecanumSpeedInput[3],
     },
-  
     output: {
-      front_left: Number(jsonValue["front_left_output"]),
-      front_right: Number(jsonValue["front_right_output"]),
-      rear_left: Number(jsonValue["rear_left_output"]),
-      rear_right: Number(jsonValue["rear_right_output"]),
+      front_left: mecanumSpeedOutput[0],
+      front_right: mecanumSpeedOutput[1],
+      rear_left: mecanumSpeedOutput[2],
+      rear_right: mecanumSpeedOutput[3],
     },
   };
 
-  chartT.series[0].setData([
+  chart1.series[0].setData([
     wheelData.input.front_left,
     wheelData.input.front_right,
     wheelData.input.rear_left,
     wheelData.input.rear_right,
   ]);
 
-  chartT.series[1].setData([
+  chart1.series[1].setData([
     wheelData.output.front_left,
     wheelData.output.front_right,
     wheelData.output.rear_left,
@@ -81,6 +96,38 @@ function plotJSON(jsonValue) {
   ]);
 }
 
+function updateTextBoxes(data) {
+  xInput = Number(data.input_x);
+  yInput = Number(data.input_y);
+  xOutput = Number(data.output_x);
+  yOutput = Number(data.output_y);
+
+  document.getElementById("input-orientation").value = (
+    Math.atan2(yInput, xInput) *
+    (180 / Math.PI)
+  ).toFixed(2);
+  document.getElementById("output-orientation").value = (
+    Math.atan2(yOutput, xOutput) *
+    (180 / Math.PI)
+  ).toFixed(2);
+  document.getElementById("value3").value = data.value3;
+  document.getElementById("value4").value = data.value4;
+  document.getElementById("value5").value = data.value5;
+}
+
+function vectorToMecanum(x, y) {
+  speed = Math.sqrt(x * x + y * y);
+  angle = Math.atan2(y, x);
+  mecanumSpeed = [
+    speed * Math.cos(angle + Math.PI / 4),
+    speed * Math.sin(angle + Math.PI / 4),
+    speed * Math.cos(angle - Math.PI / 4),
+    speed * Math.sin(angle - Math.PI / 4),
+  ];
+  return mecanumSpeed;
+}
+
+// Fonctions de récupération des données
 function getReadings() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
@@ -88,69 +135,35 @@ function getReadings() {
       var myObj = JSON.parse(this.responseText);
       console.log(myObj);
       plotJSON(myObj);
+      updateTextBoxes(myObj);
     }
   };
   xhr.open("GET", "/readings", true);
   xhr.send();
 }
 
+// Fonction de mise à jour des données
 if (!!window.EventSource) {
   var source = new EventSource("/events");
 
-  source.addEventListener(
-    "open",
-    function (e) {
-      console.log("Events Connected");
-    },
-    false
-  );
+  source.addEventListener("open", function (e) {
+    console.log("Events Connected");
+  });
 
-  source.addEventListener(
-    "error",
-    function (e) {
-      if (e.target.readyState != EventSource.OPEN) {
-        console.log("Events Disconnected");
-      }
-    },
-    false
-  );
+  source.addEventListener("error", function (e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  });
 
-  source.addEventListener(
-    "message",
-    function (e) {
-      console.log("message", e.data);
-    },
-    false
-  );
+  source.addEventListener("message", function (e) {
+    console.log("message", e.data);
+  });
 
-  source.addEventListener(
-    "new_readings",
-    function (e) {
-      console.log("new_readings", e.data);
-      var myObj = JSON.parse(e.data);
-      console.log(myObj);
-      plotJSON(myObj);
-    },
-    false
-  );
+  source.addEventListener("new_readings", function (e) {
+    console.log("new_readings", e.data);
+    var myObj = JSON.parse(e.data);
+    plotJSON(myObj);
+    updateTextBoxes(myObj);
+  });
 }
-
-const jsonData = {
-  input_orientation: "45°",
-  output_orientation: "43°",
-  value3: "50%",
-  value4: "85%",
-  value5: "OK",
-};
-
-function updateTextBoxes(data) {
-  document.getElementById("input-orientation").value = data.input_orientation;
-  document.getElementById("output-orientation").value = data.output_orientation;
-  document.getElementById("value3").value = data.value3;
-  document.getElementById("value4").value = data.value4;
-  document.getElementById("value5").value = data.value5;
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  updateTextBoxes(jsonData);
-});
